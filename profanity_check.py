790674e3b80dbd65eebe91d483da1d82ca0f1961
xjy2061 <xjy2061@gmail.com>
2017-12-16 16:39:37 +0800

    Add profanity check exercise.

---
+++ profanity_check.py
@@ -0,0 +1,23 @@
+import urllib.request
+import urllib.parse
+
+
+def read_check():
+    stream = open(r"move_quotes")
+    content = stream.read()
+    print(content)
+    profanity_check(content)
+    stream.close()
+
+
+def profanity_check(text):
+    print("============")
+    response = urllib.request.urlopen("http://www.wdylike.appspot.com/?" + urllib.parse.urlencode({"q" : text}))
+    if response.read() == b"true":
+        print("Alarm!!!")
+    else:
+        print("Clear.")
+    response.close()
+
+
+read_check()
